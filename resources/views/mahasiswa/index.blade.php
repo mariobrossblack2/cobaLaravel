3a8c6eb4a4158c2feb8fe1e7db1a8c132ca250ab
Sukirno Doank <1973203-mariobrossblack2@users.noreply.gitlab.com>
2020-05-31 22:09:24 +0700

    koneksi

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layout/main')
+
+@section('title', 'Daftar Mahasiswa')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-3">Daftar Mahasiswa</h1>
+
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nama</th>
+                        <th scope="col">NRP</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Jurusan</th>
+                        <th scope="col">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mario</td>
+                        <td>2016110062</td>
+                        <td>mario@gmail.com</td>
+                        <td>Teknik Informatika</td>
+                        <td>
+                            <a href="" class="badge badge-success">edit</a>
+                            <a href="" class="badge badge-danger">delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/about', function () {
-    $nama = 'Mario Bross Black';
-    return view('about', ['nama' => $nama]);
-});
+// Route::get('/about', function () {
+//     $nama = 'Mario Bross Black';
+//     return view('about', ['nama' => $nama]);
+// });
+
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+
+Route::get('/mahasiswa', 'MahasiswaController@index');
